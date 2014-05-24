d5e4a29d2204e7a76675cfab2d24b30055e07a33
Tim Babb <trbabb@gmail.com>
2014-05-24 13:25:21 -0700

    improve cylinder do

--- html/search/all_70.js
+++ html/search/all_70.js
@@ -1,8 +1,10 @@
 var searchData=
 [
   ['p',['p',['../structgeom_1_1_hit.html#addd25c036eb4d1edf0d47d309ab54a7c',1,'geom::Hit']]],
-  ['perlinnoise',['PerlinNoise',['../classgeom_1_1_perlin_noise.html',1,'geom']]],
+  ['p0',['p0',['../classgeom_1_1_cylinder.html#a668d099de6e768fde498a47b85adf5c4',1,'geom::Cylinder']]],
+  ['p1',['p1',['../classgeom_1_1_cylinder.html#a2eff8b7bbf0f3b8986b4289dffce5b3d',1,'geom::Cylinder']]],
   ['perlinnoise',['PerlinNoise',['../classgeom_1_1_perlin_noise.html#aed6846f95e00953c2823fe0ad92c27a6',1,'geom::PerlinNoise::PerlinNoise()'],['../classgeom_1_1_perlin_noise.html#ac0da0de5f7924882d24a0200dd79a37f',1,'geom::PerlinNoise::PerlinNoise(Random *rng)']]],
+  ['perlinnoise',['PerlinNoise',['../classgeom_1_1_perlin_noise.html',1,'geom']]],
   ['permutationmatrix',['PermutationMatrix',['../classgeom_1_1_permutation_matrix.html',1,'geom']]],
   ['permutationmatrix',['PermutationMatrix',['../classgeom_1_1_permutation_matrix.html#af6bd5099dfdc0f89049029861eb7497f',1,'geom::PermutationMatrix::PermutationMatrix()'],['../classgeom_1_1_permutation_matrix.html#ab2edfbf9c4151e9245f006460512e11e',1,'geom::PermutationMatrix::PermutationMatrix(index_t n)']]],
   ['permutationmatrix_3c_20m_20_3e',['PermutationMatrix&lt; M &gt;',['../classgeom_1_1_permutation_matrix.html',1,'geom']]],
--- html/search/variables_70.js
+++ html/search/variables_70.js
@@ -1,5 +1,7 @@
 var searchData=
 [
   ['p',['p',['../structgeom_1_1_hit.html#addd25c036eb4d1edf0d47d309ab54a7c',1,'geom::Hit']]],
+  ['p0',['p0',['../classgeom_1_1_cylinder.html#a668d099de6e768fde498a47b85adf5c4',1,'geom::Cylinder']]],
+  ['p1',['p1',['../classgeom_1_1_cylinder.html#a2eff8b7bbf0f3b8986b4289dffce5b3d',1,'geom::Cylinder']]],
   ['pt',['pt',['../classgeom_1_1_grid_iterator.html#a6557d66a1f4b24281a12de0d4e5d9ecd',1,'geom::GridIterator']]]
 ];
