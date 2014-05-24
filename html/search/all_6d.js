2ebc2146368e86c702b590c3c3bced23eeac2e5a
Tim Babb <trbabb@gmail.com>
2014-05-24 13:13:02 -0700

    update Matrix docs

--- html/search/all_6d.js
+++ html/search/all_6d.js
@@ -10,8 +10,8 @@ var searchData=
   ['mins',['mins',['../classgeom_1_1_rect.html#a32caf6df617c835be8920f40874363c9',1,'geom::Rect']]],
   ['mix',['mix',['../group__function.html#ga544e92cb874abbcc6d0636db7150eced',1,'geom']]],
   ['mtrand',['MTRand',['../classgeom_1_1_m_t_rand.html',1,'geom']]],
-  ['mtx_5faliases_5fstorage',['mtx_aliases_storage',['../group__matrix.html#gab21d5043e723f853c25239014392b5f6',1,'geom']]],
-  ['mtxcopy',['mtxcopy',['../group__matrix.html#ga33456f94a9ec6f71bf5bd3016ec787e3',1,'geom']]],
-  ['mul',['mul',['../group__matrix.html#ga1f2513740215784239349db200787eac',1,'geom::mul(Md *into, const Ma &amp;a, const Mb &amp;b)'],['../group__matrix.html#gab6fff1cabd69e3baba0e775e0afe9ca3',1,'geom::mul(const Ma &amp;a, const Mb &amp;b)']]],
+  ['mtx_5faliases_5fstorage',['mtx_aliases_storage',['../group__matrix.html#gaece56584613790b7e364cf00b93ee36a',1,'geom']]],
+  ['mtxcopy',['mtxcopy',['../group__matrix.html#ga18aa4cc96039ea7bf54d7d9fb25defe4',1,'geom']]],
+  ['mul',['mul',['../group__matrix.html#ga3f7b11d56dfce541930313e00471845a',1,'geom::mul(Matrix *into, const Matrix1 &amp;a, const Matrix2 &amp;b)'],['../group__matrix.html#ga2d61c9c78d6cfeec7f676ebbf028970c',1,'geom::mul(const Matrix1 &amp;a, const Matrix2 &amp;b)']]],
   ['mult',['mult',['../classgeom_1_1_quat.html#af668dc6f192a4bb561867444a758bc4f',1,'geom::Quat']]]
 ];
