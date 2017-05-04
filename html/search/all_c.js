d255d7201d6680ce713419cbbbcee67cb932d77b
Tim Babb <trbabb@gmail.com>
2017-05-04 00:44:18 -0700

    update dox

--- html/search/all_c.js
+++ html/search/all_c.js
@@ -4,9 +4,12 @@ var searchData=
   ['nearest',['nearest',['../classgeom_1_1_k_d_tree.html#a7102adc697f05ceeacfb9c0a47e53f11',1,'geom::KDTree::nearest(const Vec&lt; T, N &gt; &amp;p) const'],['../classgeom_1_1_k_d_tree.html#a0e9b34dabe3292a49b8ae605a34e337f',1,'geom::KDTree::nearest(const Vec&lt; T, N &gt; &amp;p)']]],
   ['nobjects',['nobjects',['../classgeom_1_1_k_d_tree.html#a8ec358712bd195d17e7439aa0c9f9ed1',1,'geom::KDTree']]],
   ['node_5farity',['node_arity',['../structgeom_1_1_k_d_tree_1_1_k_d_structure_params.html#a063fd9413f9182661f7b0fbb628d6f81',1,'geom::KDTree::KDStructureParams']]],
+  ['node_5fcount',['node_count',['../classgeom_1_1_tree_1_1_node_iterator.html#a65aa5dc1b8bfbc815ba7057c312ea154',1,'geom::Tree::NodeIterator::node_count()'],['../classgeom_1_1_tree.html#a65aa5dc1b8bfbc815ba7057c312ea154',1,'geom::Tree::node_count()']]],
   ['node_5fiterator',['node_iterator',['../classgeom_1_1_k_d_tree.html#a7f9cb14106b6a7d3bce38bf258fb4d83',1,'geom::KDTree::node_iterator()'],['../classgeom_1_1_tree.html#a043679698df71566ba0950dc26f98a92',1,'geom::Tree::node_iterator()']]],
   ['nodeiterator',['NodeIterator',['../classgeom_1_1_tree_1_1_node_iterator.html',1,'Tree&lt; NodeItem, LeafItem &gt;::NodeIterator&lt; Const &gt;'],['../class_tree_1_1_node_iterator.html',1,'Tree&lt; NodeItem, LeafItem &gt;::NodeIterator&lt; Const &gt;']]],
-  ['nodes',['nodes',['../classgeom_1_1_tree_1_1_node_iterator.html#a10f669b39f407801a814d6d073e42833',1,'geom::Tree::NodeIterator::nodes()'],['../classgeom_1_1_tree.html#a10f669b39f407801a814d6d073e42833',1,'geom::Tree::nodes()'],['../class_tree_1_1_node_iterator.html#a10f669b39f407801a814d6d073e42833',1,'Tree::NodeIterator::nodes()']]],
+  ['nodes',['nodes',['../class_tree_1_1_node_iterator.html#a10f669b39f407801a814d6d073e42833',1,'Tree::NodeIterator']]],
+  ['nodes_5fbegin',['nodes_begin',['../classgeom_1_1_tree.html#a9c89bd5f29f203eb92ed32c02dddb869',1,'geom::Tree::nodes_begin()'],['../classgeom_1_1_tree.html#ad398e3ccbb170ab1272fe6b9579a7d4d',1,'geom::Tree::nodes_begin() const']]],
+  ['nodes_5fend',['nodes_end',['../classgeom_1_1_tree.html#a14edd5e784b0845c30c46950f18e94cf',1,'geom::Tree::nodes_end()'],['../classgeom_1_1_tree.html#ae936f55f8abe9092b8a9a4d990688c42',1,'geom::Tree::nodes_end() const']]],
   ['normal',['normal',['../classgeom_1_1_plane.html#a97a283c4b6e5c00898477c179ff36c9d',1,'geom::Plane']]],
   ['normalize',['normalize',['../classgeom_1_1_spherical_harmonics.html#acd0de676568888d848beb97dcc53ae47',1,'geom::SphericalHarmonics::normalize()'],['../classgeom_1_1_zonal_harmonics.html#acd0de676568888d848beb97dcc53ae47',1,'geom::ZonalHarmonics::normalize()']]],
   ['nullspace',['nullspace',['../group__linalg.html#ga30df286717929ce22deabe6b52ae9d24',1,'geom']]]
