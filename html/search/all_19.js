f5bf279b88befe2de873d8df7256ef13d72afc2b
Tim Babb <trbabb@gmail.com>
2018-12-02 18:52:16 -0800

    update CircBuf dox

--- html/search/all_19.js
+++ html/search/all_19.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['_7ecircularbuffer',['~CircularBuffer',['../classgeom_1_1_circular_buffer.html#ab7e6556152db6439e4c6ddc594bdadc2',1,'geom::CircularBuffer']]],
   ['_7esmallstorage',['~SmallStorage',['../classgeom_1_1_small_storage.html#a0676c90dd37c4e402a609cbaa92979e1',1,'geom::SmallStorage']]]
 ];
